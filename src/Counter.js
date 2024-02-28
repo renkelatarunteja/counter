379f3c1410aa844d7a8563da0f0ebc20b7b776fd
Tarun Teja <renkelatarunteja@gmail.com>
2024-02-28 17:54:57 +0530

    changes made

---
+++ src/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount(prevCount => prevCount - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
